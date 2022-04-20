c1eda407d5761b41c5acbccd1f59362b3813c550
SHAGUN SHARMA <94694849+shagunacc@users.noreply.github.com>
2022-04-20 21:55:52 +0530

    Update Left Rotation.C

--- Left Rotation.C
+++ Left Rotation.C
@@ -1,1 +1,26 @@
-
+#include<stdio.h>
+int main()
+{
+    int n,d,a[1000000],i,j,f;
+    scanf("%d %d",&n,&d);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d ",&a[i]);
+    }
+   
+    for(i=0;i<d;i++)
+    {
+       f=a[0]; 
+        for(j=0;j<n-1;j++)
+        {
+            a[j]=a[j+1];
+            
+        }
+        a[j]=f;
+    }
+     for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+return 0;    
+}
